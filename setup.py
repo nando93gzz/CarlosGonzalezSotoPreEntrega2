cf16492941c19279744381fc633c2233c0ca0b61
nando93gzz <carlos-deoz@hotmail.com>
2023-07-10 17:29:55 -0600

    pre entrega2.5

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+
+    name="Mi primer pre-entrega2",
+    description="mi preentrega con clases",
+    author="Carlos Gonzaez",
+    author_email="carlos-deoz@hotmail.com",
+    packages=["PreEntrega"]
+
+)
